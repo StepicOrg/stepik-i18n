01f6f9ca90f78ea015e2faedfc1512b6eca89952
Nikolay Vyahhi <Nikolay.Vyahhi@stepik.org>
2016-11-07 17:30:42 -0500

    Create all.js
     All possible languages.

---
+++ all.js
@@ -0,0 +1,322 @@
+{
+  af: {
+    name: 'Afrikaans',
+    nativeName: 'Afrikaans'
+  },
+  ar: {
+    name: 'Arabic',
+    nativeName: 'العربيّة'
+  },
+  az: {
+    name: 'Azerbaijani',
+    nativeName: 'azərbaycan dili'
+  },
+  bg: {
+    name: 'Bulgarian',
+    nativeName: 'български'
+  },
+  be: {
+    name: 'Belarusian',
+    nativeName: 'беларуская'
+  },
+  bn: {
+    name: 'Bengali',
+    nativeName: 'বাংলা'
+  },
+  br: {
+    name: 'Breton',
+    nativeName: 'brezhoneg'
+  },
+  bs: {
+    name: 'Bosnian',
+    nativeName: 'bosanski'
+  },
+  ca: {
+    name: 'Catalan',
+    nativeName: 'català'
+  },
+  cs: {
+    name: 'Czech',
+    nativeName: 'česky'
+  },
+  cy: {
+    name: 'Welsh',
+    nativeName: 'Cymraeg'
+  },
+  da: {
+    name: 'Danish',
+    nativeName: 'dansk'
+  },
+  de: {
+    name: 'German',
+    nativeName: 'Deutsch'
+  },
+  el: {
+    name: 'Greek',
+    nativeName: 'Ελληνικά'
+  },
+  en: {
+    name: 'English',
+    nativeName: 'English'
+  },
+  'en-gb': {
+    name: 'British English',
+    nativeName: 'British English'
+  },
+  eo: {
+    name: 'Esperanto',
+    nativeName: 'Esperanto'
+  },
+  es: {
+    name: 'Spanish',
+    nativeName: 'Español'
+  },
+  'es-ar': {
+    name: 'Argentinian Spanish',
+    nativeName: 'español de Argentina'
+  },
+  'es-mx': {
+    name: 'Mexican Spanish',
+    nativeName: 'español de Mexico'
+  },
+  'es-ni': {
+    name: 'Nicaraguan Spanish',
+    nativeName: 'español de Nicaragua'
+  },
+  'es-ve': {
+    name: 'Venezuelan Spanish',
+    nativeName: 'español de Venezuela'
+  },
+  et: {
+    name: 'Estonian',
+    nativeName: 'eesti'
+  },
+  eu: {
+    name: 'Basque',
+    nativeName: 'Basque'
+  },
+  fa: {
+    name: 'Persian',
+    nativeName: 'فارسی'
+  },
+  fi: {
+    name: 'Finnish',
+    nativeName: 'suomi'
+  },
+  fr: {
+    name: 'French',
+    nativeName: 'français'
+  },
+  'fy-nl': {
+    name: 'Frisian',
+    nativeName: 'Frisian'
+  },
+  ga: {
+    name: 'Irish',
+    nativeName: 'Gaeilge'
+  },
+  gl: {
+    name: 'Galician',
+    nativeName: 'galego'
+  },
+  he: {
+    name: 'Hebrew',
+    nativeName: 'עברית'
+  },
+  hi: {
+    name: 'Hindi',
+    nativeName: 'Hindi'
+  },
+  hr: {
+    name: 'Croatian',
+    nativeName: 'Hrvatski'
+  },
+  hu: {
+    name: 'Hungarian',
+    nativeName: 'Magyar'
+  },
+  ia: {
+    name: 'Interlingua',
+    nativeName: 'Interlingua'
+  },
+  id: {
+    name: 'Indonesian',
+    nativeName: 'Bahasa Indonesia'
+  },
+  is: {
+    name: 'Icelandic',
+    nativeName: 'Íslenska'
+  },
+  it: {
+    name: 'Italian',
+    nativeName: 'italiano'
+  },
+  ja: {
+    name: 'Japanese',
+    nativeName: '日本語'
+  },
+  ka: {
+    name: 'Georgian',
+    nativeName: 'ქართული'
+  },
+  kk: {
+    name: 'Kazakh',
+    nativeName: 'Қазақ'
+  },
+  km: {
+    name: 'Khmer',
+    nativeName: 'Khmer'
+  },
+  kn: {
+    name: 'Kannada',
+    nativeName: 'Kannada'
+  },
+  ko: {
+    name: 'Korean',
+    nativeName: '한국어'
+  },
+  lb: {
+    name: 'Luxembourgish',
+    nativeName: 'Lëtzebuergesch'
+  },
+  lt: {
+    name: 'Lithuanian',
+    nativeName: 'Lietuviškai'
+  },
+  lv: {
+    name: 'Latvian',
+    nativeName: 'latviešu'
+  },
+  mk: {
+    name: 'Macedonian',
+    nativeName: 'Македонски'
+  },
+  ml: {
+    name: 'Malayalam',
+    nativeName: 'Malayalam'
+  },
+  mn: {
+    name: 'Mongolian',
+    nativeName: 'Mongolian'
+  },
+  my: {
+    name: 'Burmese',
+    nativeName: 'မြန်မာဘာသာ'
+  },
+  nb: {
+    name: 'Norwegian Bokmal',
+    nativeName: 'norsk (bokmål)'
+  },
+  ne: {
+    name: 'Nepali',
+    nativeName: 'नेपाली'
+  },
+  nl: {
+    name: 'Dutch',
+    nativeName: 'Nederlands'
+  },
+  nn: {
+    name: 'Norwegian Nynorsk',
+    nativeName: 'norsk (nynorsk)'
+  },
+  os: {
+    name: 'Ossetic',
+    nativeName: 'Ирон'
+  },
+  pa: {
+    name: 'Punjabi',
+    nativeName: 'Punjabi'
+  },
+  pl: {
+    name: 'Polish',
+    nativeName: 'polski'
+  },
+  pt: {
+    name: 'Portuguese',
+    nativeName: 'Português'
+  },
+  'pt-br': {
+    name: 'Brazilian Portuguese',
+    nativeName: 'Português Brasileiro'
+  },
+  ro: {
+    name: 'Romanian',
+    nativeName: 'Română'
+  },
+  ru: {
+    name: 'Russian',
+    nativeName: 'Русский'
+  },
+  sk: {
+    name: 'Slovak',
+    nativeName: 'slovenský'
+  },
+  sl: {
+    name: 'Slovenian',
+    nativeName: 'Slovenščina'
+  },
+  sq: {
+    name: 'Albanian',
+    nativeName: 'shqip'
+  },
+  sr: {
+    name: 'Serbian',
+    nativeName: 'српски'
+  },
+  'sr-latn': {
+    name: 'Serbian Latin',
+    nativeName: 'srpski (latinica)'
+  },
+  sv: {
+    name: 'Swedish',
+    nativeName: 'svenska'
+  },
+  sw: {
+    name: 'Swahili',
+    nativeName: 'Kiswahili'
+  },
+  ta: {
+    name: 'Tamil',
+    nativeName: 'தமிழ்'
+  },
+  te: {
+    name: 'Telugu',
+    nativeName: 'తెలుగు'
+  },
+  th: {
+    name: 'Thai',
+    nativeName: 'ภาษาไทย'
+  },
+  tr: {
+    name: 'Turkish',
+    nativeName: 'Türkçe'
+  },
+  tt: {
+    name: 'Tatar',
+    nativeName: 'Татарча'
+  },
+  udm: {
+    name: 'Udmurt',
+    nativeName: 'Удмурт'
+  },
+  uk: {
+    name: 'Ukrainian',
+    nativeName: 'Українська'
+  },
+  ur: {
+    name: 'Urdu',
+    nativeName: 'اردو'
+  },
+  vi: {
+    name: 'Vietnamese',
+    nativeName: 'Tiếng Việt'
+  },
+  'zh-cn': {
+    name: 'Simplified Chinese',
+    nativeName: '简体中文'
+  },
+  'zh-tw': {
+    name: 'Traditional Chinese',
+    nativeName: '繁體中文'
+  }
+};
